1fad22f98dc60198908409ced15f57c123570aa8
vasubhalodiya <vasubhalodiya30@gmail.com>
2024-04-28 18:39:39 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('.main'),
+        smooth: true,
+        lerp: 0.040
+    });
+});
