1ee53be397885136edd6fdf051e629cabdbb0b5a
vasubhalodiya <vasubhalodiya30@gmail.com>
2024-05-11 21:46:17 +0530

    update

--- js/script.js
+++ js/script.js
@@ -3,3 +3,40 @@ const scroll = new LocomotiveScroll({
     smooth: true,
     lerp: 0.040
 });
+
+// const disabledKeys = ["u"];
+
+// const showAlert = (e) => {
+//   e.preventDefault();
+//   return alert("This feature is restricted!");
+// };
+
+// document.addEventListener("contextmenu", showAlert);
+
+// document.addEventListener("keydown", (e) => {
+//   if ((e.ctrlKey && disabledKeys.includes(e.key)) || e.key === "F12") {
+//     showAlert(e);
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
