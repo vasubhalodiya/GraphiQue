e38f75ecf7d359c2097e4057899cfdc7da0203fb
vasubhalodiya <vasubhalodiya30@gmail.com>
2024-05-23 16:02:34 +0530

    update

--- js/script.js
+++ js/script.js
@@ -19,4 +19,57 @@ const scroll = new LocomotiveScroll({
 //   }
 // });
 
-/** @format */
+// page preloader animation
+
+const svg = document.getElementById("svg");
+const tl = gsap.timeline({
+  onComplete: () => {
+    document.body.style.overflow = "visible";
+  },
+});
+
+const curve = "M0 502S195 620 365 466s331-81 444 4 228 123 376-21V0H0Z";
+const flat = "M0 502S195 620 365 466s331-81 444 4 228 123 376-21V0H0Z";
+
+tl.from(".loader-wrap-heading h1", {
+  delay: 0.5,
+  y: 200,
+  skewY: 20,
+}).to(".loader-wrap-heading h1", {
+  delay: 0.5,
+  y: -200,
+  skewY: 20,
+});
+tl.to(svg, {
+  duration: 0,
+  attr: { d: curve },
+  ease: "power2.easeIn",
+}).to(svg, {
+  duration: 0,
+  attr: { d: flat },
+  ease: "power2.easeOut",
+});
+tl.to(".loader-wrap", {
+  y: -500,
+});
+tl.to(".loader-wrap", {
+  zIndex: -1,
+  display: "none",
+});
+tl.from(
+  ".main",
+  {
+    y: 100,
+    opacity: 0,
+  },
+  "-=0.9"
+);
+
+tl.from(
+  "nav",
+  {
+    duration: 1,
+    opacity: 0,
+  },
+  "-=1.2"
+);
